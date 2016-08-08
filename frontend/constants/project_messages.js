c04f004e8cf726a0a46414c98a6c5b028d9da69a
Robert Taussig <robert.taussig@gmail.com>
2016-08-08 17:56:23 -0400

    Finish start project page, begin saved project page

--- frontend/constants/project_messages.js
+++ frontend/constants/project_messages.js
@@ -1,14 +1,14 @@
 module.exports = {
   'basics header': "Let's get started.",
-  'basics': "The title of your project will impact its place in history. Pick a title, image, goal, campaign duration, and category.",
+  'basics': "The title of your project will impact its role in history. Pick a title, image, goal, campaign duration, and category.",
   'rewards header': "Add as many rewards as you wish.",
   'rewards': "You can delete them at any time. Don't forget to save them before moving on by clicking on the checkmark.",
   'story header': "If history is written by the victors...",
-  'story': "Be the victor.",
+  'story': "Be the victor? Here's your chance to convince potential funders why they should support your cause.",
   'about_you header': "Tell us about yourself.",
-  'about_you': "If history has taught us one thing, it's that the people behind an idea are more important than the idea itself.",
-  'account header': "Account",
-  'account': "Account text",
-  'preview header': "Preview",
-  'preview': "Preview text",
+  'about_you': "If we've learned one thing, it's that the person behind an idea is more important than the idea itself.",
+  'account header': "Stay informed.",
+  'account': "Provision of your contact information will be used to notify you of a successful campaign.",
+  'preview header': "How your project will appear to others:",
+  'preview': "This isn't your last chance to edit your project, but it is the last chance to edit it before it is made public."
 };
