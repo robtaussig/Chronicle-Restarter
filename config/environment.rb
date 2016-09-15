5ffba029cb0eaa0059668b5b7b81bfe7757f6fc1
robtaussig <robert.taussig@gmail.com>
2016-09-15 16:53:47 -0400

    Setup caching

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require 'rack-cache'
 
+Rails.application.configure do
+  config.middleware.use Rack::Cache,
+     verbose:     true,
+     metastore:   'file:/var/cache/rack/meta',
+     entitystore: 'file:/var/cache/rack/body'
+end
 # Initialize the Rails application.
 Rails.application.initialize!
